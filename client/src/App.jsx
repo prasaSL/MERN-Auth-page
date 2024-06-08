58be27a1a73f04347b2e8cc6c4c0ea82045eaf79
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-08 11:54:42 +0530

    Add react-router-dom package and implement routing in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
-    <div className='text-red-500 fill-red-300'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
