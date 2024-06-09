042bf95442159b65c73dec5f386d02a6e9498206
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 14:41:03 +0530

    Add user sign up functionality

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,3 +1,18 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
 export const test = (req, res) => {
     res.send('Hello World!');
+}
+
+export const signUp = async (req, res) => {
+    const {name,email,password} = req.body;
+    const hashPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({name,email,password:hashPassword});
+    try {
+        await newUser.save();
+        res.status(201).json({message: 'User created successfully'});
+    } catch (error) {
+        res.status(500).json({message: 'Error: ', error});
+    }
 }
