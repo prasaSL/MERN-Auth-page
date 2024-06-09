ac09d201ecfa4998c270921b0303571cfa3a175e
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 13:03:04 +0530

    Add route and middleware for API

--- api/index.js
+++ api/index.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import router from './routes/route.js';
+
+
 dotenv.config();
 const port = 5000;
 const app = express();
 
+app.use(express.json());
+
+app.use('/api/',router);
 mongoose.connect(process.env.MONGO_DB_CONNECTION).then(() => {
   console.log('Connected to MongoDB');
 }).catch((error) => {
   console.log('Error: ', error);
 });
+
+
+
 
 
 app.listen(port, () => {
