d129e2bd6d7095a8b0d658426c80af8fc833c2be
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 22:08:47 +0530

    Add error handling and loading state to SignUp form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,22 +4,38 @@ import { Link }  from 'react-router-dom'
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 const handleChange = (e) => {
   setFormData({...formData, [e.target.id]: e.target.value});
 }
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-  console.log(formData);
-  const response = await fetch('/api/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
-  })
-  const data = await response.json();
-  console.log(data);
+  try{
+    setLoading(true);
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    const data = await response.json();
+    console.log(data);
+    setLoading(false);
+    setError(false);
+
+    if(data.success == false){
+      setError(true);
+    }
+  }
+  catch(error){
+    setLoading(false);
+    setError(true);
+    setError(error);
+  }
+  
 };
 
   return (
@@ -29,7 +45,7 @@ const handleSubmit = async (e) => {
         <input type="text" placeholder="username" className="bg-slate-100 p-3 rounded-lg" id="name" onChange={handleChange}/>
         <input type="email" placeholder="email" className="bg-slate-100 p-3 rounded-lg" id="email" onChange={handleChange}/>
         <input type="password" placeholder="password" className="bg-slate-100 p-3 rounded-lg" id="password" onChange={handleChange}/>
-    <button className="bg-slate-900 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">sign up</button>
+    <button className="bg-slate-900 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80" disabled={loading}>{loading ? 'loading...' : 'sign up'    }</button>
       </form >
       <div className=" flex gap-2 mt-4">
 
@@ -38,10 +54,12 @@ const handleSubmit = async (e) => {
             <span className="text-blue-500" > sign in</span>
             
             </Link>
-          
         </p>
+       
       </div>
-
+      <div>
+          {error ? <p className="text-red-500 mt-5">Something went wrong</p> : null}
+        </div>
     </div>
   )
 }
