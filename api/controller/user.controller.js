78d033ac0a34ab559fb82475a5a7ac4c472e274b
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 22:08:38 +0530

    Add success field to user creation response

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -11,9 +11,11 @@ export const signUp = async (req, res , next) => {
     const newUser = new User({name,email,password:hashPassword});
     try {
         await newUser.save();
-        res.status(201).json({message: 'User created successfully'});
+        res.status(201).json({message: 'User created successfully'
+    , success: true});
     } catch (error) {
-        res.status(500).json({message: 'Error: ', error});
+        res.status(500).json({message: 'Error: ', error
+        , success: false});
         next(error);
     }
 }
