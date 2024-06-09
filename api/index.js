f9c6539e7cd9e831fb4f74c6d051eb8261f43248
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 11:20:26 +0530

    Add MongoDB connection and package.json

--- api/index.js
+++ api/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const port = 5000;
 const app = express();
 
+mongoose.connect(process.env.MONGO_DB_CONNECTION);
 
 app.listen(port, () => {
-  console.log('Server is running on port ${port}');
+  console.log('Server is running on port '+port);
 });
