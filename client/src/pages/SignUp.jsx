ee11049214439362a7a945522c42ccb0771fd361
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-09 21:21:20 +0530

    Add form functionality to SignUp page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
 import { Link }  from 'react-router-dom'
 
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+const handleChange = (e) => {
+  setFormData({...formData, [e.target.id]: e.target.value});
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  console.log(formData);
+  const response = await fetch('/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  const data = await response.json();
+  console.log(data);
+};
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7 "> sign up</h1>
-      <form className="flex flex-col gap-3">
-        <input type="text" placeholder="username" className="bg-slate-100 p-3 rounded-lg" id="username"/>
-        <input type="email" placeholder="email" className="bg-slate-100 p-3 rounded-lg" id="email"/>
-        <input type="password" placeholder="password" className="bg-slate-100 p-3 rounded-lg" id="password"/>
+      <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" className="bg-slate-100 p-3 rounded-lg" id="name" onChange={handleChange}/>
+        <input type="email" placeholder="email" className="bg-slate-100 p-3 rounded-lg" id="email" onChange={handleChange}/>
+        <input type="password" placeholder="password" className="bg-slate-100 p-3 rounded-lg" id="password" onChange={handleChange}/>
     <button className="bg-slate-900 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">sign up</button>
-      </form>
+      </form >
       <div className=" flex gap-2 mt-4">
 
         <p>Already have an account? 
