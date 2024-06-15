a223a6e9fa525d691d1a1ecddebe3df499219aa2
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-16 01:57:52 +0530

    Add sign-in functionality with JWT authentication

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const test = (req, res) => {
     res.send('Hello World!');
@@ -19,3 +22,30 @@ export const signUp = async (req, res , next) => {
         next(error);
     }
 }
+
+export const  signIn = async (req, res, next) => {
+
+    const {email,password} = req.body;
+    try {
+        const user = await User.findOne({email:email});
+        if(!user){
+            res.status(404).json({message: 'wrong user credentials' , success: false});
+        }
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if(!passwordIsValid){
+            res.status(401).json({message: 'wrong user credentials', success: false});
+        }
+      const  token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: 86400});
+      const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_JWT_SECRET, {expiresIn: 86400});
+
+        res.cookies('token', token, {httpOnly: true, secure: true, sameSite: 'true'});
+        res.cookies('refreshToken', refreshToken, {httpOnly: true, secure: true, sameSite: 'true'});
+        res.status(200).json({message: 'User signed in successfully', success: true,user: user});
+    }
+    catch (error) {
+        res.status(500).json({message: 'Error: ', error
+        , success: false});
+        next(error);
+    }
+}
+
