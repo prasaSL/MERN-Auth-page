c9f3c77eff88d17ec69988b7ab8671c5f6275f70
prasaSL <73846278+prasaSL@users.noreply.github.com>
2024-06-16 13:36:31 +0530

    Fix error handling and add cookies to sign in response

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -24,28 +24,25 @@ export const signUp = async (req, res , next) => {
 }
 
 export const  signIn = async (req, res, next) => {
-
     const {email,password} = req.body;
     try {
         const user = await User.findOne({email:email});
         if(!user){
-            res.status(404).json({message: 'wrong user credentials' , success: false});
+           return res.status(404).json({message: 'wrong user credentials' , success: false});
         }
         const passwordIsValid = bcrypt.compareSync(password, user.password);
         if(!passwordIsValid){
-            res.status(401).json({message: 'wrong user credentials', success: false});
+          return  res.status(401).json({message: 'wrong user credentials', success: false});
         }
-      const  token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: 86400});
-      const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_JWT_SECRET, {expiresIn: 86400});
-
-        res.cookies('token', token, {httpOnly: true, secure: true, sameSite: 'true'});
-        res.cookies('refreshToken', refreshToken, {httpOnly: true, secure: true, sameSite: 'true'});
-        res.status(200).json({message: 'User signed in successfully', success: true,user: user});
+        const  token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: 86400});
+        const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_JWT_SECRET, {expiresIn: 86400});
+        const {password: userPassword, ...userWithoutPassword} = user._doc;
+        res.cookie('token', token, {httpOnly: true, secure: true, sameSite: 'strict'});
+        res.cookie('refreshToken', refreshToken, {httpOnly: true, secure: true, sameSite: 'strict'});
+      return  res.status(200).json({message: 'User signed in successfully', success: true,user: userWithoutPassword});
     }
     catch (error) {
-        res.status(500).json({message: 'Error: ', error
-        , success: false});
-        next(error);
+        console.log(error);
+        res.status(500).json({message: 'Error: ', error, success: false});
     }
 }
-
