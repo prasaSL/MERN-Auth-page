2447399b0ad138e89631fb7db6243f77ba9b92d4
prasaSL <prasadmadushan09@gmail.com>
2024-07-07 11:07:15 +0530

    chore: Update Redux store to include redux-persist for state persistence

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer  from './user/userSlice';
 
-const reducer = {
-    user: userReducer,
-};
+import storage from 'redux-persist/lib/storage';
+import { persistStore , persistReducer } from 'redux-persist';
+
+const rootReducer = combineReducers({user: userReducer});
+const persistConfig = {
+    key: 'root',
+    version:1,
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig ,rootReducer);
 
 export const store = configureStore({
-    reducer,
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
 
+export const  persistor = persistStore(store);
